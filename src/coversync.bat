0be3ecd5e05aab2cc525d9c7003e4f9cd5274b1e
meine-buecherei <westerheim@meine-buecherei.de>
2018-08-29 20:36:39 +0200

    Neue Coverbilder

--- src/coversync.bat
+++ src/coversync.bat
@@ -1,11 +1,12 @@
 @rem Neue Coverbilder in Homepage importieren und hochladen
-set mypath=%~dp0
+@set mypath=%~dp0
 @echo %mypath%
-cd /D %mypath%
+@cd /D %mypath%
+@echo "Coverbilder konvertieren..."
 git pull
-cd src
 call bundle exec ruby import_bookcovers.rb
 cd ..
+@echo "Änderungen der Homepage hochladen..."
 git add .
 git commit -m "Neue Coverbilder"
 git push
